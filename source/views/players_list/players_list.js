dd2f16368cdcad686d674bb2aa2623ce7a11c27b
starostin <starostinoleg89@mail.ru>
2014-01-19 22:59:42 +0200

    a

--- source/views/players_list/players_list.js
+++ source/views/players_list/players_list.js
@@ -8,14 +8,58 @@
 RAD.view("view.players_list", RAD.Blanks.ScrollableView.extend({
     url: 'source/views/players_list/players_list.html',
     events: {
-        'click .add-player': 'openAddPlayer'
+        'click .add-player': 'openAddPlayer',
+        'click .add': 'addPlayer',
+        'click .cancel': 'notAddPlayer',
+        'webkitTransitionEnd .top': 'insertPlayerInList'
     },
+    transitionOnce: 0,
     openAddPlayer: function(){
         console.log('=--=-==--=-=-=-=-=')
         var rectangle = this.el.querySelector('.add-player-rectangle');
-        var div = document.createElement('div');
-        div.classList.add('top');
-        rectangle.appendChild(div);
-        rectangle.classList.add('rotation')
+        var top = rectangle.querySelector('.top');
+        var add = rectangle.querySelector('.add');
+        var cancel = rectangle.querySelector('.cancel');
+        var playersList = this.el.querySelector('.players-list');
+        top.classList.remove('hidden');
+        top.querySelector('.add').classList.remove('hidden');
+        top.querySelector('.cancel').classList.remove('hidden');
+        rectangle.classList.add('rotation');
+        playersList.style.marginTop = '75px';
+    },
+    addPlayer: function(){
+        console.log('===============================')
+        this.transitionOnce = 0;
+        var rectangle = this.el.querySelector('.add-player-rectangle');
+        var top = rectangle.querySelector('.top');
+        var playersList = this.el.querySelector('.players-list');
+//        top.classList.add('super-rotate');
+        playersList.style.marginTop = '25px';
+        top.classList.add('player');
+        top.querySelector('.add').classList.add('hidden');
+        top.querySelector('.cancel').classList.add('hidden');
+    },
+    notAddPlayer: function(){
+        var rectangle = this.el.querySelector('.add-player-rectangle');
+        var playersList = this.el.querySelector('.players-list');
+        rectangle.classList.remove('rotation');
+        playersList.style.marginTop = '0';
+    },
+    insertPlayerInList: function(){
+        console.log('-------------------------------------')
+        this.transitionOnce++;
+        if(this.transitionOnce > 1){
+            return;
+        }
+        var rectangle = this.el.querySelector('.add-player-rectangle');
+        var top = rectangle.querySelector('.top');
+        var li = document.createElement('li');
+        li.classList.add('player');
+        top.classList.add('hidden');
+        top.classList.remove('player');
+        rectangle.classList.remove('rotation');
+        top.classList.remove('super-rotate');
+        $('li.player').eq(0).before(li);
+        this.refreshScroll();
     }
 }));
